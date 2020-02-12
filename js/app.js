57887829f2d92fdac76e5eb4d805c60d0d5e6aea
ChristianLievrouw <celievrouw1@gmail.com>
2020-02-12 16:06:41 -0600

    added js date

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log('This is connected and working');
+
+var yourAge = prompt('How old are you?');
+var greeting;
+if(yourAge > 21){
+    greeting = 'Welcome to Beer for you!!!';
+}
+else {
+    greeting = 'Please find us when you of age!!!'
+}
+console.log(greeting);
+document.write(greeting);
+
+var today = new Date();
+var geeting;
+
+document.write(greeting + 'Today is ' + today);
+
+
+
+
+
